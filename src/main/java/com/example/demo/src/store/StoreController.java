6af5b4bdc247f3ef5f8e5290d2ec1a299682f1a7
SY-Mina <sylim9387@naver.com>
2021-12-19 00:06:52 +0900

    Store Setting

--- src/main/java/com/example/demo/src/store/StoreController.java
+++ src/main/java/com/example/demo/src/store/StoreController.java
@@ -28,26 +28,26 @@ import static com.example.demo.utils.ValidationRegex.isRegexEmail;
 public class StoreController {
     // *********************** 동작에 있어 필요한 요소들을 불러옵니다. *************************
 
-//    final Logger logger = LoggerFactory.getLogger(this.getClass()); // Log를 남기기: 일단은 모르고 넘어가셔도 무방합니다.
-//
-//    @Autowired  // 객체 생성을 스프링에서 자동으로 생성해주는 역할. 주입하려 하는 객체의 타입이 일치하는 객체를 자동으로 주입한다.
-//    // IoC(Inversion of Control, 제어의 역전) / DI(Dependency Injection, 의존관계 주입)에 대한 공부하시면, 더 깊이 있게 Spring에 대한 공부를 하실 수 있을 겁니다!(일단은 모르고 넘어가셔도 무방합니다.)
-//    // IoC 간단설명,  메소드나 객체의 호출작업을 개발자가 결정하는 것이 아니라, 외부에서 결정되는 것을 의미
-//    // DI 간단설명, 객체를 직접 생성하는 게 아니라 외부에서 생성한 후 주입 시켜주는 방식
-//    private final StoreProvider storeProvider;
-//    @Autowired
-//    private final StoreService storeService;
-//    @Autowired
-//    private final JwtService jwtService; // JWT부분은 7주차에 다루므로 모르셔도 됩니다!
-//
-//
-//    public StoreController(StoreProvider storeProvider, StoreService storeService, JwtService jwtService) {
-//        this.storeProvider = storeProvider;
-//        this.storeService = storeService;
-//        this.jwtService = jwtService; // JWT부분은 7주차에 다루므로 모르셔도 됩니다!
-//    }
-//
-//    // ******************************************************************************
-//
+    final Logger logger = LoggerFactory.getLogger(this.getClass()); // Log를 남기기: 일단은 모르고 넘어가셔도 무방합니다.
+
+    @Autowired  // 객체 생성을 스프링에서 자동으로 생성해주는 역할. 주입하려 하는 객체의 타입이 일치하는 객체를 자동으로 주입한다.
+    // IoC(Inversion of Control, 제어의 역전) / DI(Dependency Injection, 의존관계 주입)에 대한 공부하시면, 더 깊이 있게 Spring에 대한 공부를 하실 수 있을 겁니다!(일단은 모르고 넘어가셔도 무방합니다.)
+    // IoC 간단설명,  메소드나 객체의 호출작업을 개발자가 결정하는 것이 아니라, 외부에서 결정되는 것을 의미
+    // DI 간단설명, 객체를 직접 생성하는 게 아니라 외부에서 생성한 후 주입 시켜주는 방식
+    private final StoreProvider storeProvider;
+    @Autowired
+    private final StoreService storeService;
+    @Autowired
+    private final JwtService jwtService; // JWT부분은 7주차에 다루므로 모르셔도 됩니다!
+
+
+    public StoreController(StoreProvider storeProvider, StoreService storeService, JwtService jwtService) {
+        this.storeProvider = storeProvider;
+        this.storeService = storeService;
+        this.jwtService = jwtService;
+    }
+
+    // ******************************************************************************
+
 
 }
